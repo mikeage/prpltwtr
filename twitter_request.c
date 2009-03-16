544bba22cb192ac28ae86311569753c3d00570f9
neaveru <devnull@localhost>
2009-03-16 19:25:06 +0000

    Revert take 2

--- twitter_request.c
+++ twitter_request.c
@@ -42,8 +42,6 @@ typedef struct {
 	TwitterSendRequestFunc success_func;
 	TwitterSendRequestFunc error_func;
 	gpointer user_data;
-	char *request_text; //TEMP
-	GString *response;
 } TwitterSendRequestData;
 
 typedef struct
@@ -60,16 +58,18 @@ typedef struct
 void twitter_send_request_multipage_do(PurpleAccount *account,
 		TwitterMultiPageRequestData *request_data);
 
-void twitter_response_cb(const gchar *url_text, gsize len, const gchar *error_message,
-		TwitterSendRequestData *request_data)
+void twitter_send_request_cb(PurpleUtilFetchUrlData *url_data, gpointer user_data,
+		const gchar *url_text, gsize len,
+		const gchar *error_message)
 {
+	TwitterSendRequestData *request_data = user_data;
 	if (error_message)
 	{
 		printf("Request_cb error: %s\n", error_message);
 		//TODO: handle
 	} else {
 		xmlnode *node = xmlnode_from_str(url_text, len);
-		printf("Response: %s\n", url_text);
+		printf("Request_cb data: %s\n", url_text);
 		if (!node)
 		{
 			//TODO: handle
@@ -87,86 +87,8 @@ void twitter_response_cb(const gchar *url_text, gsize len, const gchar *error_me
 			g_free(node);
 		}
 	}
-}
-void twitter_send_request_cb(PurpleUtilFetchUrlData *url_data, gpointer user_data,
-		const gchar *url_text, gsize len,
-		const gchar *error_message)
-{
-	twitter_response_cb(url_text, len, error_message, (TwitterSendRequestData *) user_data);
 	g_free(user_data);
 }
-
-const gchar *twitter_send_request_response_content(const gchar *response, int len)
-{
-	static gchar *content_length_token = "\r\nContent-Length: ";
-	gchar *content_length_token_pos = NULL;
-	int content_length_token_len = strlen(content_length_token);
-	const gchar *content = NULL;
-
-	if ((content_length_token_pos = strstr(response, content_length_token)) != NULL)
-	{
-		if (len <= (content_length_token_pos - response) + content_length_token_len)
-			return NULL;
-		int content_length_expected = -1;
-		content_length_expected = atoi(content_length_token_pos + content_length_token_len);
-		if (content_length_expected == -1)
-			return NULL;
-		if (content_length_expected == 0 && content_length_token_pos[content_length_token_len] != '0')
-			return NULL;
-
-		content = g_strstr_len(response, len, "\r\n\r\n");
-		if (content == NULL)
-			return NULL;
-		content += 4;
-
-		if (len - (content - response) < content_length_expected)
-			return NULL;
-		return content;
-	}
-	return NULL;
-
-}
-
-void twitter_send_request_receive_data(gpointer data, PurpleSslConnection *gsc,
-		PurpleInputCondition cond)
-{
-	TwitterSendRequestData *request_data = data;
-	const gchar *response_content;
-	int len;
-	static char buf[4096];
-
-	while((len = purple_ssl_read(gsc, buf, sizeof(buf))) > 0) {
-		request_data->response = g_string_append_len(request_data->response, buf, len);
-	}
-
-	response_content = twitter_send_request_response_content(request_data->response->str, request_data->response->len);
-
-	if (response_content != NULL)
-	{
-		twitter_response_cb(response_content, request_data->response->len - (response_content - request_data->response->str),
-				NULL,
-				request_data);
-	} else {
-		return;
-	}
-
-	g_string_free(request_data->response, TRUE);
-	g_free(request_data);
-
-	purple_ssl_close(gsc);
-}
-void twitter_send_request_connected(gpointer data, PurpleSslConnection *gsc,
-		PurpleInputCondition cond)
-{
-	TwitterSendRequestData *request_data = data;
-	purple_ssl_input_add(gsc, twitter_send_request_receive_data, data);
-	printf("Writing: %s\n", request_data->request_text);
-	purple_ssl_write(gsc, request_data->request_text, strlen(request_data->request_text));
-
-	g_free(request_data->request_text);
-	request_data->request_text = NULL;
-}
-
 
 void twitter_send_request(PurpleAccount *account, gboolean post,
 		const char *url, const char *query_string, 
@@ -200,18 +122,15 @@ void twitter_send_request(PurpleAccount *account, gboolean post,
 			query_string  && post ? strlen(query_string) : 0,
 			query_string && post ? query_string : "");
 
-	request_data->request_text = request;
-	request_data->response = g_string_sized_new(4096);
-
+	printf("Request: %s\n", request);
 	g_free(auth_text_b64);
-
-	purple_ssl_connect(account, "www.twitter.com",
-			443, twitter_send_request_connected,
-			NULL, request_data);
-
+	purple_util_fetch_url_request(url, TRUE,
+			"Mozilla/4.0 (compatible; MSIE 5.5)", TRUE, request, FALSE,
+			twitter_send_request_cb, request_data);
+	g_free(request);
 }
 
-void twitter_send_request_multipage_cb(PurpleAccount *account, xmlnode *node, gpointer user_data)
+void twitter_send_request_mutlipage_cb(PurpleAccount *account, xmlnode *node, gpointer user_data)
 {
 	TwitterMultiPageRequestData *request_data = user_data;
 	xmlnode *child = node->child;
@@ -243,7 +162,7 @@ void twitter_send_request_multipage_do(PurpleAccount *account,
 
 	twitter_send_request(account, FALSE,
 			request_data->url, full_query_string,
-			twitter_send_request_multipage_cb, NULL,
+			twitter_send_request_mutlipage_cb, NULL,
 			request_data);
 	g_free(full_query_string);
 }
@@ -265,4 +184,5 @@ void twitter_send_request_multipage(PurpleAccount *account,
 
 	twitter_send_request_multipage_do(account, request_data);
 }
+
 
