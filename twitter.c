bd2980d3f91cda1cfba0f51874e44a1ea8a7d098
neaveru <neaveru@gmail.com>
2010-03-15 14:38:48 -0400

    Changed to show versio v0.5

--- twitter.c
+++ twitter.c
@@ -2042,7 +2042,7 @@ static PurplePluginInfo info =
 	PURPLE_PRIORITY_DEFAULT,				 /* priority */
 	TWITTER_PROTOCOL_ID,					     /* id */
 	"Twitter Protocol",					      /* name */
-	"0.4.1",						   /* version */
+	"0.5.0",						   /* version */
 	"Twitter Protocol Plugin",				  /* summary */
 	"Twitter Protocol Plugin",				  /* description */
 	"neaveru <neaveru@gmail.com>",		     /* author */
